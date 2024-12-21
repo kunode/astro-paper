d6f2eaca88a16f769dd8e67c3a25e819fa7bc67e
Kunode <xkmobi@gmail.com>
2024-12-21 17:33:00 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  website: "https://xkd.me/", // replace this with your deployed domain
+  author: "XKD",
+  profile: "https://xkd.me/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "kunode",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
